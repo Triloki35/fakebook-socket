5a68bef2e2c5102e6c93d02e5321c292a4f0c319
Triloki Nath <trilokinath35@gmail.com>
2024-01-10 12:48:19 +0530

    initialized git in socket

---
+++ index.js
@@ -0,0 +1,86 @@
+const io = require("socket.io")(8800, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+  // on connect
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    console.log(users);
+    io.emit("getUser", users); // emit users list
+  });
+
+  //   send and get msg
+  socket.on("sendMsg", ({ senderId, receiverId, data }) => {
+    // console.log(senderId + " " + receiverId + " " + text);
+    console.log(data);
+    console.log(users);
+    const user = users.find((user) => user.userId == receiverId);
+    console.log(user);
+    io.to(user?.socketId).emit("getMsg", data);
+    console.log("end");
+  });
+
+  // Notification
+  socket.on("Notification", (data) => {
+    // console.log(data);
+    // console.log(users);
+    // console.log(data);
+    const user = users.find((user) => user.userId === data.receiverId);
+    console.log(user);
+    io.to(user?.socketId).emit("Notification", data);
+  });
+
+  // friend-request
+  socket.on("send-friendRequest", (data) => {
+    const user = users.find((user) => user.userId === data.userId);
+    io.to(user?.socketId).emit("get-friendRequest");
+  });
+
+  //******calling************
+
+  socket.on("callUser", ({ friendId, signal, from, audio, video }) => {
+    // console.log("callUser");
+    console.log("Audio = "+audio);
+    console.log("Video = "+video);
+    const friend = users.find((user) => user.userId === friendId);
+		io.to(friend?.socketId).emit("callUser", { signal, from, audio:audio, video:video});
+	});
+
+	socket.on("answerCall", (data) => {
+    // console.log(data);
+    const user = users.find((user) => user.userId === data.to);
+		io.to(user?.socketId).emit("callAccepted", data.signal)
+	});
+
+  socket.on("endCall",({friendId})=>{
+    console.log("endCall");
+    console.log(friendId);
+    console.log(users);
+    const friend = users.find((user) => user.userId === friendId);
+    console.log(friend);
+    io.to(friend?.socketId).emit("endCall");
+  })
+
+  // on disconnect
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    removeUser(socket.id);
+    io.emit("getUser", users);
+  });
+});
